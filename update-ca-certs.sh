eb55b0e678cdc4d946c9c9534334994f127b6242
tf318 <16547399+tf318@users.noreply.github.com>
2022-01-03 21:29:11 +0100

    Put the cert overlay logic in its own script
    Fixes #1 so that the Homebrew Channel's original startup script remains unmodified. Also add more explanatory material to the script's output.

--- update-ca-certs.sh
+++ update-ca-certs.sh
@@ -2,32 +2,83 @@
 
 # Explicitly remove the old expired X3 certificate from the trust store
 # Download two new LetsEncrypt certificates
-# Update the start-devmode.sh script to overlay this data over the readonly filesystem and reload CA cert config on boot
-# Tam Oct 2021
+# Create a post-boot script to overlay this cert data over the readonly filesystem and reload CA cert config on boot
+# Tam Oct 2021, updates Jan 2020
+# https://github.com/tf318/lg
 
 export PURPOSE="Overlay our custom CA certificate configuration (to replace outdated CA certs) and update trust store"
 
-export DEV_MODE_SCRIPT=/media/cryptofs/apps/usr/palm/services/com.palmdts.devmode.service/start-devmode.sh
+export STARTUP_SCRIPTS_DIR=/var/lib/webosbrew/init.d
+
+export CERT_FIX_SCRIPT=${STARTUP_SCRIPTS_DIR}/overlay-letsencrypt-ca-certs-fix
 export CERT_FIX_DIR=/home/certfix-overlay
 
-if ! grep -q "${PURPOSE}" ${DEV_MODE_SCRIPT}; then
+if [ ! -d "${STARTUP_SCRIPTS_DIR}" ] ; then
+cat << EOM
+----------
+FIX FAILED
+----------
 
-	mkdir -p ${CERT_FIX_DIR}/etc/
-	mkdir -p ${CERT_FIX_DIR}/usr_share_ca-certificates/
-	mkdir -p ${CERT_FIX_DIR}/work-etc/
-	mkdir -p ${CERT_FIX_DIR}/work-usr_share_ca-certificates/
+Error: Homebrew Channel init.d directory does not exist
 
-	cat /etc/ca-certificates.conf | sed '/^mozilla\/DST_Root_CA_X3.crt$/ s/./!&/' > ${CERT_FIX_DIR}/etc/ca-certificates.conf
-	curl -k https://letsencrypt.org/certs/lets-encrypt-r3.pem --output ${CERT_FIX_DIR}/usr_share_ca-certificates/lets-encrypt-r3.crt
-	curl -k https://letsencrypt.org/certs/isrgrootx1.pem --output ${CERT_FIX_DIR}/usr_share_ca-certificates/isrgrootx1.crt
-	echo "lets-encrypt-r3.crt" >> ${CERT_FIX_DIR}/etc/ca-certificates.conf
-	echo "isrgrootx1.crt" >> ${CERT_FIX_DIR}/etc/ca-certificates.conf
+       ${STARTUP_SCRIPTS_DIR}
 
-	echo "" >> ${DEV_MODE_SCRIPT}
-	echo "# ${PURPOSE}" >> ${DEV_MODE_SCRIPT}
-	echo "mount -t overlay overlay -o lowerdir=/etc,upperdir=${CERT_FIX_DIR}/etc,workdir=${CERT_FIX_DIR}/work-etc /etc" >> ${DEV_MODE_SCRIPT}
-	echo "mount -t overlay overlay -o lowerdir=/usr/share/ca-certificates,upperdir=${CERT_FIX_DIR}/usr_share_ca-certificates,workdir=${CERT_FIX_DIR}/work-usr_share_ca-certificates /usr/share/ca-certificates" >> ${DEV_MODE_SCRIPT}
-	echo "update-ca-certificates" >> ${DEV_MODE_SCRIPT}
-	echo "" >> ${DEV_MODE_SCRIPT}
-	reboot
+Before running this script, ensure you have rooted your TV.
+
+To root your TV, visit https://rootmy.tv/ in your TV's browser.
+
+To learn more about this script, visit https://github.com/tf318/lg
+
+EOM
+exit 1
+fi
+
+if [ ! -f "${CERT_FIX_SCRIPT}" ]; then
+
+        mkdir -p ${CERT_FIX_DIR}/etc/
+        mkdir -p ${CERT_FIX_DIR}/usr_share_ca-certificates/
+        mkdir -p ${CERT_FIX_DIR}/work-etc/
+        mkdir -p ${CERT_FIX_DIR}/work-usr_share_ca-certificates/
+
+        cat /etc/ca-certificates.conf | sed '/^mozilla\/DST_Root_CA_X3.crt$/ s/./!&/' > ${CERT_FIX_DIR}/etc/ca-certificates.conf
+
+        echo "Downloading LetsEncrypt CA Certificates..."
+        echo
+
+        curl -k https://letsencrypt.org/certs/lets-encrypt-r3.pem --output ${CERT_FIX_DIR}/usr_share_ca-certificates/lets-encrypt-r3.crt
+        curl -k https://letsencrypt.org/certs/isrgrootx1.pem --output ${CERT_FIX_DIR}/usr_share_ca-certificates/isrgrootx1.crt
+        echo "lets-encrypt-r3.crt" >> ${CERT_FIX_DIR}/etc/ca-certificates.conf
+        echo "isrgrootx1.crt" >> ${CERT_FIX_DIR}/etc/ca-certificates.conf
+
+        echo
+        echo "Creating startup certificate overlay script..."
+
+        echo "#!/bin/bash" > ${CERT_FIX_SCRIPT}
+        echo "# ${PURPOSE}" >> ${CERT_FIX_SCRIPT}
+        echo "mount -t overlay overlay -o lowerdir=/etc,upperdir=${CERT_FIX_DIR}/etc,workdir=${CERT_FIX_DIR}/work-etc /etc" >> ${CERT_FIX_SCRIPT}
+        echo "mount -t overlay overlay -o lowerdir=/usr/share/ca-certificates,upperdir=${CERT_FIX_DIR}/usr_share_ca-certificates,workdir=${CERT_FIX_DIR}/work-usr_share_ca-certificates /usr/share/ca-certificates" >> ${CERT_FIX_SCRIPT}
+        echo "update-ca-certificates" >> ${CERT_FIX_SCRIPT}
+        echo "" >> ${CERT_FIX_SCRIPT}
+
+        chmod a+x ${CERT_FIX_SCRIPT}
+
+        echo
+        echo "LetsEncrypt certificate fix configuration complete. Rebooting to apply fix..."
+
+        reboot
+else
+cat << EOM2
+-------------------
+FIX ALREADY APPLIED
+-------------------
+
+It looks like the fix was already in place.
+
+If you are sure it was not working, please delete the following file:
+
+        ${CERT_FIX_SCRIPT}
+
+Then re-run this script ($0).
+
+EOM2
 fi
